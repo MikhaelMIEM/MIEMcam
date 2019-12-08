eccbb9530c89a6622ebe94a51fda84bd8cfbf9c6
MikhaelMIEM <mvnikolenko@edu.hse.ru>
2019-12-08 17:01:15 +0300

    add rtsp proxying

--- example.py
+++ example.py
@@ -1,41 +1,75 @@
 from ONVIFCameraControl import ONVIFCameraControl as OCC
+from RtspProxiesPool import RtspProxiesPool
 from time import sleep
+import keyboard
+import cv2
+
+cams = [
+    {
+        "addr":
+            {
+                "ip": "172.18.200.52",
+                "port": 80
+            },
+        "user": "admin",
+        "password": "Supervisor",
+        "room": "504",
+        "uid": "fawuifq",
+        "name": "214"
+    },
+    {
+        "addr":
+            {
+                "ip": "172.18.200.53",
+                "port": 80
+            },
+        "user": "admin",
+        "password": "Supervisor",
+        "room": "504",
+        "uid": "fawuifq1",
+        "name": "2134"
+    },
+]
 
 if __name__ == '__main__':
+
+    rtspProxiesPool = RtspProxiesPool()
+
+    for cam in cams:
+        rtspProxiesPool.add_proxy(cam)
+        uid = cam['uid']
+        port = rtspProxiesPool.get_port(uid)  # More information at this function docstring
+
+    print('Press enter to end proxying')
+    input()
+
     # to watch stream type in command line 'vlc rtsp://camera_ip'
-    cam = OCC(("172.18.200.51", 80), "admin", "Supervisor", "wsdl")
-    #cam = OCC(("192.168.15.42", 80), "admin", "Supervisor", "wsdl")
-    #cam = OCC(("172.18.199.41", 8899), "admin", "Supervisor", "wsdl")
+    # cam = OCC(("172.18.200.53", 80), "admin", "Supervisor", "wsdl")
+    # cam = OCC(("192.168.15.42", 80), "admin", "Supervisor", "wsdl")
+    # cam = OCC(("172.18.199.41", 8899), "admin", "Supervisor", "wsdl")
 
-
-    cam.move_continuous((1, 1, 1))
-    sleep(1)
-    cam.set_preset(1)
-    cam.move_continuous((-1, -1, -1))
-    sleep(1)
-    cam.stop()
-    cam.set_preset(2)
-    sleep(1)
-    cam.goto_preset(1)
-    sleep(2)
-    cam.goto_preset(2)
-    sleep(2)
-    cam.go_home()
-    # print(cam.get_presets()[0])
+    # while True:
+    #     key = keyboard.read_key()
+    #     if key == 'j':
+    #         print("down")
+    #         cam.move_continuous((0, -1, 0))
+    #         sleep(1)
+    #         cam.stop()
     #
-    # cam.set_brightness(0)
-    # sleep(1)
-    # cam.set_brightness(100)
-    # sleep(1)
-    # cam.set_brightness(50)
+    #     if key == "k":
+    #         print("up")
+    #         cam.move_continuous((0, 1, 0))
+    #         sleep(1)
+    #         cam.stop()
     #
-    # cam.move_focus_continuous(1)
-    # sleep(4)
-    # cam.stop_focus()
-    # cam.move_focus_continuous(-1)
-    # sleep(4)
-    # cam.stop_focus()
-    # cam.move_focus_absolute(0.9, 0.1)
-    # cam.set_focus_mode('AUTO')
-
-    #print(cam.get_stream_uri(protocol='RTSP', stream='RTP-Multicast'))
+    #     if key == "l":
+    #         print("right")
+    #         cam.move_continuous((1, 0, 0))
+    #         sleep(1)
+    #         cam.stop()
+    #
+    #     if key == "h":
+    #         print("left")
+    #         cam.move_continuous((-1, 0, 0))
+    #         sleep(1)
+    #         cam.stop()
